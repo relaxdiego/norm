e467eb83418238c74fd76993682d157f102dc2a9
Mark Maglana <mmaglana@gmail.com>
2012-05-30 10:15:30 +0800

    Add more capabilities to TestCaseTranslator

--- lib/test_case_translator.rb
+++ lib/test_case_translator.rb
@@ -14,8 +14,10 @@ class TestCaseTranslator
 
     ruby_code = ""
     in_header =
-      in_body =
-      in_variables = false
+      in_variables =
+      in_preconditions =
+      in_script =
+      in_cleanup = false
     variables = {}
 
     while position < string.size
@@ -24,24 +26,43 @@ class TestCaseTranslator
       if section_marker = chunk[/\A(\s*[A-Z].*:)/, 1]
         type = section_marker.strip.gsub(/:$/, '')
 
-        if in_header && type == TEST_CASE
+        if type == TEST_CASE && in_header
           ruby_code = end_test_case_header(ruby_code)
+          ruby_code = end_steps(ruby_code)
           ruby_code = end_test_case(ruby_code)
           ruby_code << "\n"
-        elsif in_header && [VARIABLES, PRECONDITIONS, SCRIPT, CLEANUP].include?(type)
+        elsif [VARIABLES, PRECONDITIONS, SCRIPT, CLEANUP].include?(type) && in_header
           ruby_code = end_test_case_header(ruby_code)
+        elsif type == CLEANUP && (in_variables || in_preconditions || in_script)
+          ruby_code << "  ensure\n"
+        elsif type == TEST_CASE && (in_variables || in_preconditions || in_script)
+          ruby_code = end_steps(ruby_code)
+          ruby_code = end_test_case(ruby_code)
+          ruby_code << "\n"
+        elsif type == TEST_CASE && in_cleanup
+          ruby_code = end_cleanup_section(ruby_code)
+          ruby_code = end_test_case(ruby_code)
+          ruby_code << "\n"
         end
 
+        in_header =
+          in_variables =
+          in_preconditions =
+          in_script =
+          in_cleanup = false
+
         if type == TEST_CASE
-          ruby_code << "TestCases.add(/^"
+          ruby_code << "test_cases.add(/^"
           in_header = true
         elsif type == VARIABLES
-          in_header = false
-          in_body = in_variables = true
-          variables = {}
+          in_variables = true
+          variables    = {}
         elsif type == PRECONDITIONS
-          in_variables = false
           in_preconditions = true
+        elsif type == SCRIPT
+          in_script = true
+        elsif type == CLEANUP
+          in_cleanup = true
         else
           raise "Unknown section marker '#{ type }'"
         end
@@ -51,13 +72,14 @@ class TestCaseTranslator
           ruby_code << "#{ test_case_id.strip } "
           position += test_case_id.size
         end
-      elsif in_variables && variable_def = chunk[/\A(\s*.+=.+)/, 1]
-        name, value = split_variable_def(variable_def)
+      elsif in_variables && variable_def = chunk[/\A(\s*\**\s*.+=.+)/, 1]
+        str = variable_def.gsub(/\A\s*\**\s*/, '')
+        name, value = split_variable_def(str)
         variables[name] = value
         position += variable_def.size
-      elsif in_preconditions && step = chunk[/\A(\s*\* .+)/, 1]
-        rx_str = step.gsub(/^\s*\* /, '')
-        ruby_code << "  Steps.call(\"#{ translate_vars(rx_str, variables) }\")\n"
+      elsif (in_preconditions || in_script || in_cleanup) && step = chunk[/\A(\s*\**\s*.+)/, 1]
+        str = step.gsub(/\A\s*\**\s*/, '')
+        ruby_code << "    steps.call(\"#{ translate_vars(str, variables) }\")\n"
         position += step.size
       end
 
@@ -66,7 +88,13 @@ class TestCaseTranslator
 
     if in_header
       ruby_code = end_test_case_header(ruby_code)
-      in_header = false
+      ruby_code = end_steps(ruby_code)
+    end
+
+    if in_variables || in_preconditions || in_script
+      ruby_code = end_steps(ruby_code)
+    elsif in_cleanup
+      ruby_code = end_cleanup_section(ruby_code)
     end
 
     ruby_code = end_test_case(ruby_code)
@@ -76,6 +104,15 @@ class TestCaseTranslator
 
   private
 
+  def end_cleanup_section(code)
+    code << "  end\n"
+  end
+
+  def end_steps(code)
+    code << "  ensure\n  end\n"
+    code
+  end
+
   def end_test_case(code)
     code << "end\n"
     code
@@ -83,7 +120,9 @@ class TestCaseTranslator
 
   def end_test_case_header(code)
     code.strip!
-    code << "$/i) do\n"
+    code << "$/i) do |regex, string|\n"
+    code << "  regex.match(string)\n\n"
+    code << "  begin\n"
     code
   end
 
@@ -93,15 +132,10 @@ class TestCaseTranslator
   end
 
   def translate_vars(string, variables)
-    if match = /<([A-Za-z ]+)>/.match(string)
-      xlated = ""
-      (match.length-1).times do |i|
-        xlated += string.gsub("<#{ match[i+1] }>", "\" + #{ variables[match[i+1]] }.to_s + \"")
-      end
-    else
-      xlated = string + "\""
+    string.scan(/<([A-Z].*?)>/).each do |match|
+      string = string.gsub("<#{ match[0] }>", "\" + #{ variables[match[0]] }.to_s + \"")
     end
-    xlated
+    string
   end
 
 end
--- test/test_case_translator_test.rb
+++ test/test_case_translator_test.rb
@@ -10,7 +10,12 @@ Test Case:
 EOF
 
     target_file = <<-EOF
-TestCases.add(/^A user with a role of User in the system can create a project$/i) do
+test_cases.add(/^A user with a role of User in the system can create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+  ensure
+  end
 end
 EOF
 
@@ -29,7 +34,12 @@ Test Case:
 EOF
 
     target_file = <<-EOF
-TestCases.add(/^A user with a role of (.+) in the system can create a project$/i) do
+test_cases.add(/^A user with a role of (.+) in the system can create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+  ensure
+  end
 end
 EOF
 
@@ -48,10 +58,20 @@ Test Case:
 EOF
 
     target_file = <<-EOF
-TestCases.add(/^A user with a role of (.+) in the system can create a project$/i) do
+test_cases.add(/^A user with a role of (.+) in the system can create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+  ensure
+  end
 end
 
-TestCases.add(/^A user cannot create a project$/i) do
+test_cases.add(/^A user cannot create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+  ensure
+  end
 end
 EOF
     assert_equal target_file, TestCaseTranslator.new.translate(source_file)
@@ -70,8 +90,41 @@ Test Case:
 EOF
 
     target_file = <<-EOF
-TestCases.add(/^A user with a role of (.+) in the system can create a project$/i) do
-  Steps.call("I have a role of " + $1.to_s + " in the system")
+test_cases.add(/^A user with a role of (.+) in the system can create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+    steps.call("I have a role of " + $1.to_s + " in the system")
+  ensure
+  end
+end
+EOF
+
+    assert_equal target_file, TestCaseTranslator.new.translate(source_file)
+  end
+
+  def test_variable_substitution_2
+    source_file = <<-EOF
+Test Case:
+  A user with a role of (.+) in the system can create a project
+
+  Variables:
+    Role Name = $1
+
+  Preconditions:
+    * I have a role of <Role Name> in the system <Role Name>
+    * I have a role of <Role Name> in the system <Role Name>
+EOF
+
+    target_file = <<-EOF
+test_cases.add(/^A user with a role of (.+) in the system can create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+    steps.call("I have a role of " + $1.to_s + " in the system " + $1.to_s + "")
+    steps.call("I have a role of " + $1.to_s + " in the system " + $1.to_s + "")
+  ensure
+  end
 end
 EOF
 
@@ -85,78 +138,317 @@ Test Case:
 
   Variables:
     Role Name = $1
+    A Variable = 'test'
 
   Preconditions:
-    * I have a role of <Role Name> in the system <Role Name>
+    * I have a role of <Role Name> in the system <A Variable>
 EOF
 
     target_file = <<-EOF
-TestCases.add(/^A user with a role of (.+) in the system can create a project$/i) do
-  Steps.call("I have a role of " + $1.to_s + " in the system " + $1.to_s + "")
+test_cases.add(/^A user with a role of (.+) in the system can create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+    steps.call("I have a role of " + $1.to_s + " in the system " + 'test'.to_s + "")
+  ensure
+  end
 end
 EOF
 
     assert_equal target_file, TestCaseTranslator.new.translate(source_file)
   end
 
+  def test_preconditions_section
+    source_file = <<-EOF
+Test Case:
+  A user with a role of (.+) in the system can create a project
 
-# #   def test_full_test_case_translation
-# #     source_file = <<-EOF
-# # Test Case:
-# #   A user with a role of (.+) in the system can create a project
-# #
-# #   Variables:
-# #     * Role Name    = $1
-# #     * Project Name = 'test'
-# #     * My Username  = my_username
-# #     * My Password  = my_password
-# #
-# #   Preconditions:
-# #     * I have a role of <Role Name> in the system
-# #
-# #   Script:
-# #     * Click the Logout button if I'm currently logged in
-# #     * Fill in the Username field with <My Username>
-# #     * Fill in the Password field with <My Password>
-# #     * Click the Login button
-# #     * Click the Projects link
-# #     * Click the New Project button
-# #     * Fill in the Project Name field with <Project Name>
-# #     * Fill in the Project Description field with 'Test project'
-# #     * Click the Create Project button
-# #     * A project named <Project Name> should be visible in the page
-# #     * The project should exist in the backend cloud
-# #
-# #   Cleanup:
-# #     * Delete the project named <Project Name> if it exists
-# #     * Delete my username at exit
-# #
-# #
-# # EOF
-# #
-# #     target_file = <<-EOF
-# # TestCase /^A user with a role of (.+) in the system can create a project$/i do
-# #     Steps.call("I have a role of " + $1.to_s + " in the system")
-# #
-# #     begin
-# #       Steps.call("Click the Logout button if I'm currently logged in")
-# #       Steps.call("Fill in the Username field with " + my_username.to_s)
-# #       Steps.call("Fill in the Password field with " + my_password.to_s)
-# #       Steps.call("Click the Login button")
-# #       Steps.call("Click the Projects link")
-# #       Steps.call("Click the New Project button")
-# #       Steps.call("Fill in the Project Name field with " + 'test'.to_s)
-# #       Steps.call("Fill in the Project Description field with 'Test project'")
-# #       Steps.call("Click the Create Project button")
-# #       Steps.call("A project named " + 'test'.to_s + " should be visible in the page")
-# #       Steps.call("The project should exist in the backend cloud")
-# #     ensure
-# #       Steps.call("Delete the project named " + 'test'.to_s + " if it exists")
-# #       Steps.call("Delete my username at exit")
-# #     end
-# #   end
-# # EOF
-# #
-# #     assert_equal target_file, TestXlator.new.translate(source_file)
-# #   end
+  Preconditions:
+    * I have a role of System Admin
+EOF
+
+    target_file = <<-EOF
+test_cases.add(/^A user with a role of (.+) in the system can create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+    steps.call("I have a role of System Admin")
+  ensure
+  end
+end
+EOF
+
+    assert_equal target_file, TestCaseTranslator.new.translate(source_file)
+  end
+
+  def test_script_section
+    source_file = <<-EOF
+Test Case:
+  A user with a role of (.+) in the system can create a project
+
+  Script:
+    * I have a role of System Admin
+EOF
+
+    target_file = <<-EOF
+test_cases.add(/^A user with a role of (.+) in the system can create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+    steps.call("I have a role of System Admin")
+  ensure
+  end
+end
+EOF
+
+    assert_equal target_file, TestCaseTranslator.new.translate(source_file)
+  end
+
+  def test_cleanup_section
+    source_file = <<-EOF
+Test Case:
+  A user with a role of (.+) in the system can create a project
+
+  Script:
+    * I have a role of System Admin
+
+  Cleanup:
+    * Delete my username at exit
+EOF
+
+    target_file = <<-EOF
+test_cases.add(/^A user with a role of (.+) in the system can create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+    steps.call("I have a role of System Admin")
+  ensure
+    steps.call("Delete my username at exit")
+  end
+end
+EOF
+
+    assert_equal target_file, TestCaseTranslator.new.translate(source_file)
+  end
+
+  def test_full_test_case_translation
+    source_file = <<-EOF
+Test Case:
+  A user with a role of (.+) in the system can create a project
+
+  Variables:
+    * Role Name = $1
+    * Project Name = 'test'
+    * My Username  = my_username
+    * My Password  = my_password
+
+  Preconditions:
+    * I have a role of <Role Name> in the system
+
+  Script:
+    * Click the Logout button if I'm currently logged in
+    * Fill in the Username field with <My Username>
+    * Fill in the Password field with <My Password>
+    * Click the Login button
+    * Click the Projects link
+    * Click the New Project button
+    * Fill in the Project Name field with <Project Name>
+    * Fill in the Project Description field with 'Test project'
+    * Click the Create Project button
+    * A project named <Project Name> should be visible in the page
+    * The project should exist in the backend cloud
+
+  Cleanup:
+    * Delete the project named <Project Name> if it exists
+    * Delete my username at exit
+
+
+EOF
+
+    target_file = <<-EOF
+test_cases.add(/^A user with a role of (.+) in the system can create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+    steps.call("I have a role of " + $1.to_s + " in the system")
+    steps.call("Click the Logout button if I'm currently logged in")
+    steps.call("Fill in the Username field with " + my_username.to_s + "")
+    steps.call("Fill in the Password field with " + my_password.to_s + "")
+    steps.call("Click the Login button")
+    steps.call("Click the Projects link")
+    steps.call("Click the New Project button")
+    steps.call("Fill in the Project Name field with " + 'test'.to_s + "")
+    steps.call("Fill in the Project Description field with 'Test project'")
+    steps.call("Click the Create Project button")
+    steps.call("A project named " + 'test'.to_s + " should be visible in the page")
+    steps.call("The project should exist in the backend cloud")
+  ensure
+    steps.call("Delete the project named " + 'test'.to_s + " if it exists")
+    steps.call("Delete my username at exit")
+  end
+end
+EOF
+
+    assert_equal target_file, TestCaseTranslator.new.translate(source_file)
+  end
+
+  def test_translation_of_two_full_test_cases
+    source_file = <<-EOF
+Test Case:
+  A user with a role of (.+) in the system can create a project
+
+  Variables:
+    * Role Name    = $1
+    * Project Name = 'test'
+    * My Username  = my_username
+    * My Password  = my_password
+
+  Preconditions:
+    * I have a role of <Role Name> in the system
+
+  Script:
+    * Click the Logout button if I'm currently logged in
+    * Fill in the Username field with <My Username>
+    * Fill in the Password field with <My Password>
+    * Click the Login button
+    * Click the Projects link
+    * Click the New Project button
+    * Fill in the Project Name field with <Project Name>
+    * Fill in the Project Description field with 'Test project'
+    * Click the Create Project button
+    * A project named <Project Name> should be visible in the page
+    * The project should exist in the backend cloud
+
+  Cleanup:
+    * Delete the project named <Project Name> if it exists
+    * Delete my username at exit
+
+Test Case:
+  A user with a role of (.+) in the system cannot create a project
+
+  Variables:
+    Role Name    = $1
+    Project Name = 'test'
+    My Username  = my_username
+    My Password  = my_password
+
+  Preconditions:
+    * I have a role of <Role Name> in the system
+
+  Script:
+    * Click the Logout button if I'm currently logged in
+    * Fill in the Username field with <My Username>
+    * Fill in the Password field with <My Password>
+    * Click the Login button
+    * The Projects link should not exist
+
+  Cleanup:
+    * Delete my username at exit
+EOF
+
+    target_file = <<-EOF
+test_cases.add(/^A user with a role of (.+) in the system can create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+    steps.call("I have a role of " + $1.to_s + " in the system")
+    steps.call("Click the Logout button if I'm currently logged in")
+    steps.call("Fill in the Username field with " + my_username.to_s + "")
+    steps.call("Fill in the Password field with " + my_password.to_s + "")
+    steps.call("Click the Login button")
+    steps.call("Click the Projects link")
+    steps.call("Click the New Project button")
+    steps.call("Fill in the Project Name field with " + 'test'.to_s + "")
+    steps.call("Fill in the Project Description field with 'Test project'")
+    steps.call("Click the Create Project button")
+    steps.call("A project named " + 'test'.to_s + " should be visible in the page")
+    steps.call("The project should exist in the backend cloud")
+  ensure
+    steps.call("Delete the project named " + 'test'.to_s + " if it exists")
+    steps.call("Delete my username at exit")
+  end
+end
+
+test_cases.add(/^A user with a role of (.+) in the system cannot create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+    steps.call("I have a role of " + $1.to_s + " in the system")
+    steps.call("Click the Logout button if I'm currently logged in")
+    steps.call("Fill in the Username field with " + my_username.to_s + "")
+    steps.call("Fill in the Password field with " + my_password.to_s + "")
+    steps.call("Click the Login button")
+    steps.call("The Projects link should not exist")
+  ensure
+    steps.call("Delete my username at exit")
+  end
+end
+EOF
+
+    assert_equal target_file, TestCaseTranslator.new.translate(source_file)
+  end
+
+  def test_translation_of_two_incomplete_tests
+    source_file = <<-EOF
+Test Case:
+  A user with a role of (.+) in the system can create a project
+
+  Variables:
+    * Role Name    = $1
+    * Project Name = 'test'
+    * My Username  = my_username
+    * My Password  = my_password
+
+  Preconditions:
+    * I have a role of <Role Name> in the system
+
+  Script:
+    * Click the Logout button if I'm currently logged in
+    * Fill in the Username field with <My Username>
+    * Fill in the Password field with <My Password>
+    * Click the Login button
+    * Click the Projects link
+    * Click the New Project button
+    * Fill in the Project Name field with <Project Name>
+    * Fill in the Project Description field with 'Test project'
+    * Click the Create Project button
+    * A project named <Project Name> should be visible in the page
+    * The project should exist in the backend cloud
+
+Test Case:
+  A user with a role of (.+) in the system cannot create a project
+EOF
+
+    target_file = <<-EOF
+test_cases.add(/^A user with a role of (.+) in the system can create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+    steps.call("I have a role of " + $1.to_s + " in the system")
+    steps.call("Click the Logout button if I'm currently logged in")
+    steps.call("Fill in the Username field with " + my_username.to_s + "")
+    steps.call("Fill in the Password field with " + my_password.to_s + "")
+    steps.call("Click the Login button")
+    steps.call("Click the Projects link")
+    steps.call("Click the New Project button")
+    steps.call("Fill in the Project Name field with " + 'test'.to_s + "")
+    steps.call("Fill in the Project Description field with 'Test project'")
+    steps.call("Click the Create Project button")
+    steps.call("A project named " + 'test'.to_s + " should be visible in the page")
+    steps.call("The project should exist in the backend cloud")
+  ensure
+  end
+end
+
+test_cases.add(/^A user with a role of (.+) in the system cannot create a project$/i) do |regex, string|
+  regex.match(string)
+
+  begin
+  ensure
+  end
+end
+EOF
+
+    assert_equal target_file, TestCaseTranslator.new.translate(source_file)
+  end
 end
